ccf2b8db88dafeabd20957a2211fb088fa030caa
Klay Clarke <klayaclarke@gmail.com>
2021-11-18 22:16:08 -0500

    simplified solution using 'not equal' comparison operator

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ import csv
 
 with open('weather_data.csv', mode='r') as csv_data:
     data = csv.reader(csv_data)
-    temporary_list = []
-    for row in data:
-        temp = row[1]
-        temporary_list.append(temp)
-    temporary_list.remove('temp')
     temperatures = []
-    for num in temporary_list:
-        converted_num = int(num)
-        temperatures.append(converted_num)
-    print(temperatures)
+    for row in data:
+        temp_num = row[1]
+        if temp_num != 'temp':
+            temperatures.append(int(temp_num))
+
+
+print(temperatures)
