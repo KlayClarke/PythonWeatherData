48d022f78c340aff65b2bf682a4c3890693dae9f
Klay Clarke <klayaclarke@gmail.com>
2021-11-18 22:09:02 -0500

    isolate temperature data from csv format and place each value in a temperature list as integers

--- main.py
+++ main.py
@@ -2,5 +2,13 @@ import csv
 
 with open('weather_data.csv', mode='r') as csv_data:
     data = csv.reader(csv_data)
+    temporary_list = []
     for row in data:
-        print(row)
+        temp = row[1]
+        temporary_list.append(temp)
+    temporary_list.remove('temp')
+    temperatures = []
+    for num in temporary_list:
+        converted_num = int(num)
+        temperatures.append(converted_num)
+    print(temperatures)
