aa5ddf195310235ca3359dcb24bfc08ec3837579
Klay Clarke <klayaclarke@gmail.com>
2021-11-18 23:00:38 -0500

    use pandas module to read csv format files

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import csv
+import pandas
 
-with open('weather_data.csv', mode='r') as csv_data:
-    data = csv.reader(csv_data)
-    temperatures = []
-    for row in data:
-        temp_num = row[1]
-        if temp_num != 'temp':
-            temperatures.append(int(temp_num))
+# with open('weather_data.csv', mode='r') as csv_data:
+#     data = csv.reader(csv_data)
+#     temperatures = []
+#     for row in data:
+#         temp_num = row[1]
+#         if temp_num != 'temp':
+#             temperatures.append(int(temp_num))
 
-
-print(temperatures)
+data = pandas.read_csv('weather_data.csv')
+print(data)
